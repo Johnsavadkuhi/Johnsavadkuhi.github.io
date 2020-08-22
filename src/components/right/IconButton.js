7291e09e6a022dfc025f3252cae64d154f05378f
Johnsavadkuhi <physitime@gmail.com>
2020-08-22 17:31:00 +0430

    update

--- src/components/right/IconButton.js
+++ src/components/right/IconButton.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {   faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import {faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
 function Iconbutton({ children }) {
   return (
     <button className="tag">
-      {children}   <FontAwesomeIcon icon={faAngleDown} />
+      {children}   <FontAwesomeIcon icon={faCaretDown} />
     </button>
   );
 }
