3769dea7a57b8e08aaecbcf9208047c058f4d59d
Johnsavadkuhi <physitime@gmail.com>
2020-08-23 01:54:53 +0430

    create MainFooter component

---
+++ src/components/right/MainFooter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Mainfooter() {
+  return (
+    <div className="footer-main">
+      <span>Tag 1 </span>
+      <span>Tag 2 </span>
+      <span>Tag 3 </span>
+    </div>
+  );
+}
+
+export default Mainfooter;
