29ba52ec5748542b1b2c78bcd114f14b1d55dd8c
Johnsavadkuhi <physitime@gmail.com>
2020-08-22 16:27:28 +0430

    update

--- src/components/right/Main.js
+++ src/components/right/Main.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Formula from '../../img/icons8_pi.svg' ;
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {   faCalculator } from '@fortawesome/free-solid-svg-icons'
 
 function Main({children}) {
   return (
     <div className="main">
       <div className="main-header">
         <div className="main-header-left">
-          <span>Arithmetic</span>
-          <span>•</span>
-          <span>Real Problem</span>
+          <FontAwesomeIcon style={{color:"white" , fontSize:"12px"}} icon={faCalculator}/>  <span style={{marginLeft:"5px"}}> Arithmetic</span> <span>  </span>   <span style={{color:"white"}}> • </span> <span>Real Problem</span>
         </div>
         <div className="main-header-right">
           <img src={Formula} alt="Formula" /> Formula
