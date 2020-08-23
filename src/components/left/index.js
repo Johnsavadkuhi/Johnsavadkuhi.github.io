48dc7ca32d43282c5a40f3df13d242b5eefd7a89
Johnsavadkuhi <physitime@gmail.com>
2020-08-23 11:55:15 +0430

    create index

---
+++ src/components/left/index.js
@@ -0,0 +1,4 @@
+export { default as Circleprogress } from "./CircleProgress";
+export { default as Bottom } from "./Bottom";
+export { default as Report } from "./Report";
+export { default as Top } from "./Top";
---
+++ src/components/right/index.js
@@ -0,0 +1,6 @@
+export {default as Header} from './Header'; 
+export {default as IconButton} from './IconButton' ; 
+export {default as InsideMain}from './InsideMain'; 
+export {default as Main} from './Main' ; 
+export {default as MainFooter} from './MainFooter'; 
+export {default as MainRight } from './MainRight'; 
