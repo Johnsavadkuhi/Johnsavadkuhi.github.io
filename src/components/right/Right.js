49d2768840dd3ce41afa714df853c637bcb899b3
Johnsavadkuhi <physitime@gmail.com>
2020-08-23 11:57:08 +0430

    create component

---
+++ src/components/right/Right.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Header , InsideMain ,Main } from ".";
+
+function Right() {
+  return (
+    <>
+      <div className="right">
+        <Header />
+        <Main>
+          <InsideMain />
+        </Main>
+      </div>
+    </>
+  );
+}
+
+export default Right;
