8405227382db1d5f062ba5fcf171373670ae7f20
Johnsavadkuhi <physitime@gmail.com>
2020-08-23 12:06:12 +0430

    move chart to new component

--- src/components/right/MainLeft.js
+++ src/components/right/MainLeft.js
@@ -1,31 +1,10 @@
-import React , {useEffect , useState }from "react";
-import { Line } from "react-chartjs-2";
+import React from "react";
 import MainFooter  from "./MainFooter";
+import LinearChart from "./LinearChart";
 
 function MainLeft() {
 
-    const [chartData, setChartData] = useState({});
-
-  const chart = (canvas) => {
-    setChartData({
-      labels: ["2015", "2016", "2017", "2018"],
-      datasets: [
-        {
-          data: [20, 60 , 40,80],
-          backgroundColor: "transparent",
-          borderWidth: 1,
-          borderColor: "white",
-          lineTension: 0,
-          pointBackgroundColor:"white" 
-        },
-      ],
-    });
-  };
-
-  useEffect(() => {
-    chart();
-  }, []);
-
+ 
   return (
     <div className="main-left">
       <p>
@@ -36,79 +15,8 @@ function MainLeft() {
         year old could drink in a day to meet or exceed the recommended daily
         calcium intake from these drinks alone ?
       </p>
-
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          background: "#181818",
-          marginTop: "10px",
-          borderRadius: "1%",
-        }}
-      >
-        <Line
-          data={chartData}
-          options={{
-            responsive: true,
-            legend: { display: false },
-            title: {
-              text: [
-                "Number Of Portable Media Players",
-                " ",
-                "Sold WOrldwide Each Year from 2006 to 2011",
-              ],
-              display: true,
-              fontColor: "white",
-              padding: 15,
-            },
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    lineHeight: 2,
-                    source: "auto",
-                    min: 0,
-                    padding: 10,
-                    autoSkips: false,
-                    beginAtZero: true,
-                    stepSize: 10,
-                    fontColor: "white",
-                  },
-                  gridLines: {
-                    color: "white",
-                    lineWidth: 0.2,
-                    drawTicks: false,
-                  },
-                  scaleLabel: {
-                    labelString: "Number Sold (Millions)",
-                    display: true,
-                    padding: 20,
-                  },
-                },
-              ],
-              xAxes: [
-                {
-                  ticks: {
-                    autoSkips: true,
-                    beginAtZero: true,
-                    padding: 5,
-                    fontColor: "white",
-                  },
-                  gridLines: {
-                    color: "white",
-                    lineWidth: 0.2,
-                    drawTicks: true,
-                    tickMarkLength: 10,
-                    drawOnChartArea: false,
-                  },
-                  offset: true,
-                },
-              ],
-            },
-          }}
-        />
-      </div>
-
+      
+      <LinearChart/>
       <MainFooter />
     </div>
   );
