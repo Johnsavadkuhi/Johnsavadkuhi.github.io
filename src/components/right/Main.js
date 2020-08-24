11fc32b43fb5f45a16b8d9f797ed957baf021fb0
Johnsavadkuhi <physitime@gmail.com>
2020-08-24 23:19:11 +0430

    update

--- src/components/right/Main.js
+++ src/components/right/Main.js
@@ -1,14 +1,19 @@
 import React from "react";
-import Formula from '../../img/icons8_pi.svg' ;
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {   faCalculator } from '@fortawesome/free-solid-svg-icons'
+import Formula from "../../img/icons8_pi.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalculator } from "@fortawesome/free-solid-svg-icons";
 
-function Main({children}) {
+function Main({ children }) {
   return (
     <div className="main">
       <div className="main-header">
         <div className="main-header-left">
-          <FontAwesomeIcon style={{color:"white" , fontSize:"12px"}} icon={faCalculator}/>  <span style={{marginLeft:"5px"}}> Arithmetic</span> <span>  </span>   <span style={{color:"white"}}> • </span> <span>Real Problem</span>
+          <FontAwesomeIcon
+            style={{ color: "white", fontSize: "12px" }}
+            icon={faCalculator}
+          />{" "}
+          <span style={{ marginLeft: "5px" }}> Arithmetic</span> <span> </span>{" "}
+          <span style={{ color: "white" }}> • </span> <span>Real Problem</span>
         </div>
         <div className="main-header-right">
           <img src={Formula} alt="Formula" /> Formula
