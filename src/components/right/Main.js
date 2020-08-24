982bf2b02f3e27f458fe8eb5ccd33e9e5def40bf
Johnsavadkuhi <physitime@gmail.com>
2020-08-24 23:23:32 +0430

    add new component and remove redundent codes

--- src/components/right/Main.js
+++ src/components/right/Main.js
@@ -2,23 +2,13 @@ import React from "react";
 import Formula from "../../img/icons8_pi.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalculator } from "@fortawesome/free-solid-svg-icons";
+import MainHeader from "./MainHeader";
 
 function Main({ children }) {
   return (
     <div className="main">
-      <div className="main-header">
-        <div className="main-header-left">
-          <FontAwesomeIcon
-            style={{ color: "white", fontSize: "12px" }}
-            icon={faCalculator}
-          />
-          <span style={{ marginLeft: "5px" }}> Arithmetic</span> <span> </span>{" "}
-          <span style={{ color: "white" }}> • </span> <span>Real Problem</span>
-        </div>
-        <div className="main-header-right">
-          <img src={Formula} alt="Formula" /> Formula
-        </div>
-      </div>
+    
+     <MainHeader/>
       {children}
     </div>
   );
