8bb38e5cc72d1547b2bad882de72c3c768c1b079
Johnsavadkuhi <physitime@gmail.com>
2020-08-24 23:28:46 +0430

    remove unsed imports

--- src/components/right/Main.js
+++ src/components/right/Main.js
@@ -1,9 +1,5 @@
 import React from "react";
-import Formula from "../../img/icons8_pi.svg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalculator } from "@fortawesome/free-solid-svg-icons";
 import MainHeader from "./MainHeader";
-
 function Main({ children }) {
   return (
     <div className="main">
